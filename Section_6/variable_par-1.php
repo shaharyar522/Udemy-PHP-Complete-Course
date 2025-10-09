742440a25b9acb7a9edc820ccad635ff879a005c
shaharyar522 <124666378+shaharyar522@users.noreply.github.com>
2025-10-09 19:25:19 +0500

    Section_6 start

---
+++ Section_6/variable_par-1.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$name = "shari"; //string must be in dobule qotes "" or single quotes '' 
+
+
+
+// var_dump
+
+//display detailed information about a variable, including its data type (int ,string float ,etc) and value
+
+
+//var are case sesative
+
+
+//snake case
+$full_name = 'hello';
+
+//camel case
+
+$fullName = "shari";
+
+// Pascal case 
+
+$FullName = "shari";
+
+
+
+
+
+var_dump($name);
