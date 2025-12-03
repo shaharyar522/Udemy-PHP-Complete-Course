c4f1c22607c109b8b604fc8e603f1528bfc89d33
Shahar Yar <shaharyar.khan4511@gmail.com>
2025-12-03 00:48:51 -0800

    complte topic Inharitance in php

---
+++ Section_21_OOP_Fundamentals/class_inharitance.php
@@ -0,0 +1,48 @@
+<?php
+
+class User
+{
+
+
+    public $name;
+
+    public $email;
+
+    public function __construct($name, $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+    
+    public function displayUserInfo(){
+        return "User: {$this->name}, Email: {$this->email}";
+    }
+}
+
+$user = new User('shari','shari@gmail.com');
+// echo $user->displayUserInfo();
+
+// inharitance parents ko child k ander extends kya hnva hin mtlab 
+class AdminUser extends User {
+
+    public $role;
+    
+
+    public function __construct($name, $email, $role)
+    {
+        parent::__construct($name, $email);
+        $this->role = $role;
+    }
+
+
+    public function displayAdminInfo(){
+         return "Admin : {$this->name}, Email: {$this->email}, {$this->role}";
+    }
+      
+}
+
+$admin = new AdminUser("Admin", "shari@gamil.com", "role_sahri");
+
+echo $admin->displayAdminInfo();
+
+
