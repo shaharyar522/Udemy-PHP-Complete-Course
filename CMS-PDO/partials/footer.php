57ebc3ac21f4c1334f6cd641de1dc5659daa2111
Shahar Yar <shaharyar.khan4511@gmail.com>
2025-12-22 01:53:30 -0800

    add this main section

--- CMS-PDO/partials/footer.php
+++ CMS-PDO/partials/footer.php
@@ -2,7 +2,7 @@
     <footer class="bg-light py-4">
         <div class="container text-center">
             <p class="text-muted mb-0">
-                &copy; 2045 CMS PDO System. All rights reserved by Edwin Diaz from EdwinDiaz.com
+                &copy; 2045 CMS PDO System. All rights reserved by Shahar Yar
             </p>
         </div>
     </footer>
