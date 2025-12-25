8e9035ceef0818532c353fcd0cca2e42bf3d3179
Shahar Yar <shaharyar.khan4511@gmail.com>
2025-12-25 12:22:03 -0800

    create helpers file, and create function and this function is , base url, base_path, uploads_url

---
+++ CMS-PDO/helpers.php
@@ -0,0 +1,31 @@
+<?php  
+
+
+function base_url($path = ""){
+    $protocol = isset($_SERVER['HTTPS']) && isset($_SERVER['HTTPS'])  !== "off" ? 'https://' : 'https';
+    $host = $_SERVER['HTTP_HOST'];
+    $baseUrl = $protocol . $host;
+    return $baseUrl. '/' . ltrim($path, '/');
+}
+
+
+function base_path($path = "") {
+    $rootPath = dirname(__DIR__);
+    return $rootPath . DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR);
+}
+
+function uploads_url($filename = '') {
+    return base_path('uploads/' . DIRECTORY_SEPARATOR . $filename);
+}
+
+
+function assets_url($path) {
+    return base_path('assets/').ltrim($path, '/');
+}
+
+function redirect($url) {
+    header('Location:' . base_url($url));
+    exit();
+}
+
+?>
---
+++ CMS-PDO/init.php
@@ -0,0 +1,13 @@
+<?php
+
+//Session start
+session_start();
+
+// Include helpers function 
+require_once('helpers.php');
+
+
+
+// Define Global constant
+define("APP NAME", "CMS PDO SYSTEM");
+?>
