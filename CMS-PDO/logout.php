cebddd18040e44baad7e858e6a16df8e78ce2199
Shahar Yar <shaharyar.khan4511@gmail.com>
2026-01-06 10:27:47 +0500

    complete this working right register and show dynamic data

---
+++ CMS-PDO/logout.php
@@ -0,0 +1,15 @@
+<?php  
+require_once "init.php";
+
+if(isPostRequest()){
+    session_destroy();
+    redirect("index.php");
+    exit();
+} else {
+    redirect("index.php");
+    exit();
+}
+
+
+
+?>
