4ff4d4d317a927223ad543007dc68f39fc2b48e1
Shahar Yar <shaharyar.khan4511@gmail.com>
2026-01-26 23:39:26 +0500

    complete MVC  application right now with oop conspect

--- mvc-app/app/init.php
+++ mvc-app/app/init.php
@@ -1,22 +1,40 @@
 <?php
 session_start();
 
-require_once __DIR__ . '/../config/config.php';  // config files hain 
-require_once __DIR__ . '/../config/database.php';  // database file hai
+$config = require_once __DIR__ . "/../config/config.php";
+
+if(!defined('BASE_URL')) {
+    define('BASE_URL', $config['app']['base_url']);
+}
+
+require_once __DIR__ . "/../config/database.php";
+
 
 require_once __DIR__ . '/helpers.php';
+
+
 
 spl_autoload_register(function ($class_name) {
     $paths = [
         __DIR__ . '/controllers/',
         __DIR__ . '/models/',
+        __DIR__ . '/middlewares/',
+
     ];
 
     foreach ($paths as $path) {
         $file = $path . $class_name . '.php';
-        if (file_exists($file)) {
+
+        if(file_exists($file)) {
             require_once $file;
             return;
         }
     }
+
 });
+
+
+
+
+
+
--- mvc-app/config/config.php
+++ mvc-app/config/config.php
@@ -1,6 +1,18 @@
-<?php  
+<?php
 
+return [
 
-const  BASE_URL = "http://localhost/"; 
+        'database' => [
+            'host'     => 'localhost',
+            'database' => 'mvc_app_db',
+            'username' => 'root',
+            'password' => '',
+            'port'     => 3306,
+            'charset' => 'utf8mb4'
+        ],
+        'app' => [
+            'base_url' => 'http://mvc-app.local/',
+            'debug'    => true,
+        ]
 
-?>
+    ];
